d608fd5c9ce93e7a8b8450f783d850ddf950f76e
Christopher Barbas <chrisbarbas30@gmail.com>
2023-02-25 13:48:23 -0500

    exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    for (const element of args) {
+        let index = array.indexOf(element);
+        if (index >= 0) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    if ((typeof x === 'number' && typeof y === 'number') && (x >= 0 && y >= 0)) {
+        let min = x;
+        let max = y;
+        let sum = 0;
 
+        if (x > y) {
+            min = y;
+            max = x;
+        }
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    return 'ERROR';
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
