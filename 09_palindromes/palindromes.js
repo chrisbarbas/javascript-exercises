1208eb9726c42feb7344f097cf5b62a77ba71060
Christopher Barbas <chrisbarbas30@gmail.com>
2023-02-25 15:15:20 -0500

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes =  str => {
+    let re = /[\W_]/g;
+    let lowRegStr = str.toLowerCase().replace(re, '');
+    let reverseStr = lowRegStr.split('').reverse().join('');
+    return reverseStr === lowRegStr;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
